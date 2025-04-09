d9ad74b5dc7f1ea1b609b71e78fcd42428036e2e
etern0ww <joaogabrieldasilvascarvalho@gmail.com>
2025-04-08 21:07:40 -0300

    Update q2.py

--- respostas/q2.py
+++ respostas/q2.py
@@ -2,4 +2,4 @@
 # Crie um programa que exiba as seguintes informações, cada uma em uma linha.
 print("Instituto FederaldoPiauí")
 print(" João Gabriel da silva s Carvalho")
-print("lic ciênciasdacomputação")
+print("lic ciências da computação")
